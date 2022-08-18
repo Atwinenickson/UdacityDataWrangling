da475d144654a079c7594c33c079323c96cd2fef
Atwine Nickson <atwine.nickson@sybyl.com>
2022-08-18 16:05:47 +0300

    add to pandas the tweet json txt file data

--- load_txt.py
+++ load_txt.py
@@ -2,36 +2,23 @@ import pandas as pd
 import json
 
 total_objects = []
+total_dict = []
 with open("tweet-json.txt") as file:
     for item in file:
         item = item.strip()
         total_objects.append(item)
 
 for item in total_objects:
-    print(item)
-
-
-
     pp = json.loads(item)
     r = pp['created_at']
     pr = pp['retweet_count']
     id = pp['id']
     rp = pp['favorite_count']
-    print(id)
-    print('........id......')
-
-
-    print(pr)
-    print('........id......')
-
-    print(rp)
-    print('........id......')
-
-    print(r)
-    print('........id......')
 
     my_dict = {"S.No.": [id], "Item":[pr], "Quantity": [rp], "Price": [r]}
+    total_dict.append(my_dict)
 
-    df = pd.DataFrame.from_dict(my_dict)
 
-    print(df)
+df = pd.DataFrame.from_dict(total_dict)
+
+print(df)
